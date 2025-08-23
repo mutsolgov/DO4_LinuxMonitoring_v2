554644a550aba1363bd511d0077c22d11a69bb41
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-24 02:49:19 +0300

    DO4_LinuxMonitoring

---
+++ src/01/lib_namegen.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Функция для генерации имен папок.файлов с сохранением порядка букв
+
+# generate_name <letters> <min_len>
+# Возвращает строку длиной >= min_len, содержащую каждую букву из <letters> хотя бы один раз,
+# и повторяющую букву в том же порядке (никогда не ставя буквы в обратном порядке).
+generate_name() {
+    local letters="$1"
+    local min_len="$2"
+    # нормализуем: в нижний регистр
+    letters=$(echo "$letters" | tr '[:upper:]' '[:lower:]')
+    local -a chars
+    local i
+    for (( i=0; i<${#letters}; i++ )); do
+        chars[i]="${letters:i:1}"
+    done
+
