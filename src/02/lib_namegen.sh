fb71414a777189724ad197e7770af8e491cee157
F_Muhammad <maga.mutsolgov@bk.ru>
2025-09-09 00:36:33 +0300

    DO4_LinuxMonitoring

---
+++ src/02/lib_namegen.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Функции генерации имен (каждая буква используется минимум 1 раз, порядок сохранен).
+
+# generate <letters> <min_len>
+generate_name() {
+    local letters="$1"
+    local min_len="$2"
+    letters=$(echo "$letters" | tr '[:upper:]' '[:lower:]')
+    local -a chars
+    local i
+    for ((i=0;i<${#letters};i++)); do
+        chars[i]="${letters:i:1}"
+    done
+
+    local name=""
+    for c in "${chars[@]}"; do
+        name+="$c"
+    done
+
+    local idx=0
+    # если letters пустые - возврат пустой строки
+    if [ "${#chars[@]}" -eq 0 ]; then
+        echo ""
+        return
+    fi
+
+    while [ "${#name}" -lt "$min_len" ]; do
+        name+="${chars[idx]}"
+        idx=$(( (idx + 1) % ${#chars[@]} ))
+    done
+
+    echo "$name"
+}
+
