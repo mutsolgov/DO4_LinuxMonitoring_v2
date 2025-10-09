568cac9964d23104f9ee432874de916b5640d0d7
F_Muhammad <maga.mutsolgov@bk.ru>
2025-10-09 22:50:16 +0300

    DO4_LinuxMonitoring

--- src/03/lib_io.sh
+++ src/03/lib_io.sh
@@ -9,3 +9,17 @@ info() {
     echo "$*"
 }
 
+# Подтверждение пользователя (если CONFIRM_ALL=1, подтверждение пропускается)
+# confirm "Сообщение" -> return 0 если подтверждено, иначе return 1
+confirm() {
+    local msg="${1:-Продолжить?}"
+    if [ "${CONFIRM_ALL:-0}" -eq 1 ]; then
+        return 0
+    fi
+    read -r -p "$msg [y/N]: " ans
+    case "$ans" in
+        [Yy]|[Yy][Ee][Ss]) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
